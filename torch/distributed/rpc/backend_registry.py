7422e12eca66d0e4f2103c4b84440ba0507e40d9
Howard Huang <howardhuang@fb.com>
2022-04-12 11:34:30 -0700

    [Dynamic RPC] Add graceful shutdown for dynamic RPC members
    ghstack-source-id: a5ca52fdc5ec03acf67af682f04c113e4139a295
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/74561

--- torch/distributed/rpc/backend_registry.py
+++ torch/distributed/rpc/backend_registry.py
@@ -5,9 +5,11 @@ from typing import Dict, List, Set, Tuple
 
 import torch
 import torch.distributed as dist
+from torch.distributed.rpc.utils import group_membership_management
 
 from . import api
 from . import constants as rpc_constants
+from . import utils
 
 
 BackendValue = collections.namedtuple(
@@ -363,40 +365,29 @@ def _tensorpipe_init_backend_handler(store, name, rank, world_size, rpc_backend_
         return agent
     # initialization for dynamic rpc (ranks can join and leave)
     else:
-        token_key = "RpcGroupManagementToken"
-        token_location = f"TokenOnWorker{rank}"
-        while True:
-            # Retrieve token from store to signal start of rank join/leave critical section
-            returned = store.compare_set(token_key, "", token_location).decode()
-            if returned == token_location:
-                # Construct TPAgent with empty reverse_device_map and devices
-                # these properties will be updated after initialization
-                agent = TensorPipeAgent(
-                    store,
-                    name,
-                    rank,
-                    world_size,
-                    rpc_backend_options,
-                    {},
-                    [],
-                )
-                api._init_rpc_states(agent)
+        with utils.group_membership_management(store, name):
+            # Construct TPAgent with empty reverse_device_map and devices
+            # these properties will be updated after initialization
+            agent = TensorPipeAgent(
+                store,
+                name,
+                rank,
+                world_size,
+                rpc_backend_options,
+                {},
+                [],
+            )
+            api._init_rpc_states(agent)
 
-                # Notify all workers in group this rank has joined and set devices and reverse_device_map
+            try:
+                # TODO: Notify all workers in group this rank has joined and set devices and reverse_device_map
                 # This is a synchronous operation that completes once all existing ranks are updated
-                _set_devices_and_reverse_device_map(agent)
-
-                # Finish initialization
-                break
-            else:
-                # Store will wait for the token to be released based on the timeout set in backend options
-                store.wait([returned])
-
-        # Update from store to signal end of rank join/leave critical section
-        store.set(token_key, "")
-        # Other will wait for this token to be set before they execute
-        store.set(token_location, "Done")
-        return agent
+                _set_devices_and_reverse_device_map(agent, rank, rpc_backend_options.device_maps)
+                pass
+            except Exception:
+                # api.shutdown()
+                raise
+            return agent
 
 register_backend(
     "TENSORPIPE",
